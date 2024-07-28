156cf6ca379a080bcc6a40e89e873d3f4120e73f
logan <logan.loganwood@gmail.com>
2024-07-28 15:47:17 +0900

    spotify controller init

---
+++ StreamShareAPI/Controllers/SpotifyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StreamShareAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SpotifyController : ControllerBase
+{
+    private readonly ILogger<SpotifyController> _logger;
+
+    public SpotifyController(ILogger<SpotifyController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return Ok("Spotify Controller");
+    }
+}
+
